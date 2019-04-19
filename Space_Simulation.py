c9bc2661cb666e88a2c7bf499e137fd3d53af663
parkerstorck13 <pstorck6@gmail.com>
2019-04-18 22:46:49 -0500

    Update Space_Simulation.py

--- Space_Simulation.py
+++ Space_Simulation.py
@@ -1,6 +1,7 @@
 import pygame, time, sys
 
 class Simulation:
+    #Initiates Pygame display window and class variables
     def __init__(self):
         pygame.init()
         self.window = pygame.display.set_mode((700, 700))
@@ -10,29 +11,35 @@ class Simulation:
         self.marks = []
         self.paused = False
 
+    #Clears the screen
     def clear(self):
         self.window.fill((0, 0, 0))
 
+    #Clears the screen and resets all objects in simulation to default values
     def reset(self):
         self.clear()
         self.rocket.reset()
 
+    #Halves all sizes and positions on screen to imitate a zoom out
     def zoomOut(self):
         self.clear()
         self.rocket.halfVars()
         for mark in self.marks:
             mark.halfVars()
 
+    #Doubles all sizes and positions on screen to imitate a zoom in
     def zoomIn(self):
         self.clear()
         self.rocket.doubleVars()
         for mark in self.marks:
             mark.doubleVars()
 
+    #Ends the simulation if user clicks GUI X, or types escape or q key
     def endSim(self):
         pygame.quit()
         sys.exit()
 
+    #Scans for user keyboard input and acclerates rocket, resets simulation, zooms in or out or quits simulation
     def getInput(self):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -56,10 +63,12 @@ class Simulation:
                     if event.key == pygame.K_i:
                         self.zoomIn()
 
+    #Redraws all past locations of rocket to show path
     def drawMarks(self):
         for mark in self.marks:
             pygame.draw.circle(self.window, (255, 255, 255), (int(mark.posX), int(mark.posY)), int(self.rocket.radius / 2), 1)
 
+    #Loops to gather user input and update simulation
     def simLoop(self):
             self.getInput()
             self.rocket.update()
@@ -74,29 +83,35 @@ class Axis:
     pass
 
 class Mark:
+    #Initiates a mark object to track the track of the rocket
     def __init__(self, X, Y):
         self.posX = X
         self.posY = Y
 
+    #Halves variables for zooming out simualtion
     def halfVars(self):
         self.posX, self.posY = (self.posX - 350) / 2 + 350, (self.posY - 350) / 2 + 350
 
+    #Doubles variables for zooming in simulation
     def doubleVars(self):
         self.posX, self.posY = (self.posX - 350) * 2 + 350, (self.posY - 350) * 2 + 350
 
 class Rocket:
+    #Initiates rocket object representing object in space
     def __init__(self):
         self.posX, self.posY = 350, 350
         self.velX, self.velY = 0, 0
         self.accX, self.accY = 0, 0
         self.radius = 10
 
+    #Updates velocity and position variables of rocket
     def update(self):
         self.velX += self.accX
         self.velY += self.accY
         self.posX += self.velX
         self.posY += self.velY
 
+    #Halves variables for zooming out simulation
     def halfVars(self):
         self.posX, self.posY = (self.posX - 350) / 2 + 350, (self.posY - 350) / 2 + 350
         self.velX /= 2
@@ -105,6 +120,7 @@ class Rocket:
         self.accY /= 2
         self.radius /= 2
 
+    #Doubles variables for zooming in simulation
     def doubleVars(self):
         self.posX, self.posY = (self.posX - 350) * 2 + 350, (self.posY - 350) * 2 + 350
         self.velX *= 2
@@ -113,24 +129,29 @@ class Rocket:
         self.accY *= 2
         self.radius *= 2
 
+    #Accelerates rocket upward when up key is pressed
     def accelUp(self):
         self.accY -= 0.25
 
+    #Accelerates rocket downward when down key is pressed
     def accelDown(self):
         self.accY += 0.25
 
+    #Accelerates rocket right when right key is pressed
     def accelRight(self):
         self.accX += 0.25
 
+    #Accelerates rocket left when left key is pressed
     def accelLeft(self):
         self.accX -= 0.25
 
+    #Resets class variables to initial values
     def reset(self):
         self.posX, self.posY = 350, 350
         self.velX, self.velY = 0, 0
         self.accX, self.accY = 0, 0
 
-
+#Inititalizes function and goes through simulation loop
 def main():
     sim = Simulation()
     while sim.running:
